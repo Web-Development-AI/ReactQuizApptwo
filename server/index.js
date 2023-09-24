3dbca0aff64e752e60f8fa8a2e858b8c18cf7ab1
Ranjan Kumar <67799181+ranjan-alt@users.noreply.github.com>
2023-09-24 15:22:34 +0000

    cors fix

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const cors = require("cors");
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
+server.use(cors());
 server.use(router);
 
 server.listen(port);
