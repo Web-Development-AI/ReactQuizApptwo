f9f4f69f0efd892c890c2f1dff732559d399e74e
Ranjan Kumar <67799181+ranjan-alt@users.noreply.github.com>
2023-09-24 14:22:09 +0000

    react quiz

---
+++ my-quiz/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ReactQuiz from "./ReactQuiz/ReactQuiz";
+
+function App() {
+  return (
+    <div className="App">
+      <ReactQuiz />
+    </div>
+  );
+}
+
+export default App;
